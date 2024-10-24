5c470206470d3e276da4711e8779fb777940517d
Marzipan <marcis.darguzs9@gmail.com>
2024-10-24 17:09:26 +0300

    Front updated
    Aded image need to implement alot

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ const App = () => {
 
   return (
     <div className="container">
-      <img src="/images/Cave.png" alt="Lucky Cards Plus Logo" className="logo" />
+      <img src="/images/Logo.png" alt="Lucky Cards Plus Logo" className="logo" />
       <h1>Lucky Cards Plus</h1>
       <button onClick={handleStart} className="start-button">Start</button>
     </div>
